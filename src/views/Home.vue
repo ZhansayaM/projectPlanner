5cbc4e0a41de504b76e91e31113bf3ec1bfe9f69
ZhansayaM <maksutzh@gmail.com>
2022-03-03 22:24:39 +0600

    show all, completed, ongoing separately done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home">
+    <!-- $event => the data that we send with emit -->
+    <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
       </div>
     </div>
   </div>
@@ -10,13 +16,15 @@
 
 <script>
 import SingleProject from "../components/SingleProject.vue";
+import FilterNav from "../components/FilterNav.vue";
 
 export default {
   name: "Home",
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
       projects: [],
+      current: "all",
     };
   },
   mounted() {
@@ -31,12 +39,20 @@ export default {
         return project.id !== id;
       });
     },
-    handleComplete(id){
-      let p = this.projects.find(project => {
-        return project.id === id
-      })
-      p.complete = !p.complete
-    }
+    handleComplete(id) {
+      let p = this.projects.find((project) => {
+        return project.id === id;
+      });
+      p.complete = !p.complete;
+    },
+  },
+  computed: {
+    filteredProjects: function () {
+      if (this.current === "all") return this.projects;
+      else if (this.current === "completed")
+        return this.projects.filter((project) => project.complete);
+      return this.projects.filter((project) => !project.complete);
+    },
   },
 };
 </script>
